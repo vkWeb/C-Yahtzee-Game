93cae6b849adbc4ad3f7801b2e41f8441106d596
Vivek Agrawal <vivekmittalagrawal@gmail.com>
2019-08-23 11:01:22 +0530

    Update Yahtzee.c

--- Yahtzee.c
+++ Yahtzee.c
@@ -49,50 +49,74 @@ int sumOfAllDice(int diceFrequency[])
 {
   int diceSum = 0;
   for (int i = 0; i < 6; ++i)
-    diceSum += (diceFrequency[i] * i + 1);
+    diceSum += (diceFrequency[i] * (i + 1));
   return diceSum;
 }
 
 void keepDice(char dice[])
 {
   int diceFrequency[6] = {0};
+  int scoreOptions[7] = {0};
+
   for (int i = 0; i < strlen(dice); ++i)
   {
     if (dice[i] != ' ')
       ++diceFrequency[(int)(dice[i] - '1')];
   }
 
-  printf("Your score options are: ");
+  printf("\nYour score options are: ");
 
-  int consecutiveCount = 0;
-  for (int i = 0; i < 6; ++i)
+  int option = 0;
+  for (int i = 0, consecutiveCount = 0; i < 6; ++i)
   {
     if (diceFrequency[i] == 1 || diceFrequency[i] == 2)
       ++consecutiveCount;
     if (diceFrequency[i] >= 3)
-      printf("-> Three of a Kind (%d points)", sumOfAllDice(diceFrequency));
+    {
+      scoreOptions[option] = sumOfAllDice(diceFrequency);
+      printf("\n%d -> Three of a Kind (%d points)", (option + 1), scoreOptions[option]);
+      ++option;
+    }
     if (diceFrequency[i] >= 4)
-      printf("\n-> Four of a Kind (%d points)", sumOfAllDice(diceFrequency));
+    {
+      scoreOptions[option] = sumOfAllDice(diceFrequency);
+      printf("\n%d -> Four of a Kind (%d points)", (option + 1), scoreOptions[option]);
+      ++option;
+    }
     if (diceFrequency[i] == 3)
     {
       for (int j = 0; j < 6; ++i)
       {
         if (diceFrequency[j] == 2)
-          printf("\n-> Full House (%d points)", 25);
+        {
+          scoreOptions[option] = 25;
+          printf("\n%d -> Full House (%d points)", (option + 1), scoreOptions[option]);
+          ++option;
+        }
       }
     }
+    if (consecutiveCount == 4 && consecutiveCount != 5)
+    {
+      scoreOptions[option] = 30;
+      printf("\n%d -> Small Straight (%d points)", (option + 1), scoreOptions[option]);
+      ++option;
+    }
+    if (consecutiveCount == 5)
+    {
+      scoreOptions[option] = 40;
+      printf("\n%d -> Straight (%d points)", (option + 1), scoreOptions[option]);
+      ++option;
+    }
     if (diceFrequency[i] == 5)
-      printf("\n-> Yahtzee (%d points)", 50);
+    {
+      scoreOptions[option] = 50;
+      printf("\n%d -> Yahtzee (%d points)", (option + 1), scoreOptions[option]);
+      ++option;
+    }
   }
-
-  if (consecutiveCount == 4)
-    printf("\n-> Small Straight (%d points)", 30);
-  if (consecutiveCount == 5)
-  {
-    printf("\n-> Small Straight (%d points)", 30);
-    printf("\n-> Straight (%d points)", 40);
-  }
-  printf("\n-> Chance (%d points)", sumOfAllDice(diceFrequency));
+  scoreOptions[option] = sumOfAllDice(diceFrequency);
+  printf("\n%d -> Chance (%d points)", (option + 1), scoreOptions[option]);
+  ++option;
 }
 
 int main(void)
@@ -106,5 +130,6 @@ int main(void)
       keepDice(dice);
     }
   }
+  printf("\n");
   return 0;
 }
